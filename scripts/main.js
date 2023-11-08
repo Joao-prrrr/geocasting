c81b5a32db9cacdda64ca50dda942fef1a952dbf
Joao-prrrr <joao.prrrr@eduge.ch>
2023-11-08 15:08:20 +0100

    done

--- scripts/main.js
+++ scripts/main.js
@@ -3,72 +3,100 @@ const myCanvas = document.querySelector('#myCanvas');
 const cameraBtn = document.querySelector('#cameraBtn');
 const btnReturn = document.querySelector('#btnReturn');
 const shootBtn = document.querySelector('#shootBtn');
+const switchCamera = document.querySelector('#switchCamera');
 
 const constraints = {
     video: {
-        width: { ideal: 1280 },
-        height: { ideal: 720 },
-        facingMode: { exact: "environment" }
+        width: { ideal: 1920 },
+        height: { ideal: 1080 },
+        facingMode: { exact: "user" }
     }
 };
 
 // navigator.mediaDevices.getUserMedia({ video: true});
 
-navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
-    myVideo.srcObject = stream;
-});
 
 // start the program
 myCanvas.style.display = "none"
 myVideo.style.display = "none"
 shootBtn.style.display = "none"
 btnReturn.style.display = "none"
+switchCamera.style.display = "none"
 cameraBtn.style.display = "block"
+
+window.addEventListener('deviceorientation',  (e) => {
+    if(e.alpha == 90 || e.alpha == 270) {
+        switchOrientation();
+    }
+})
 
 cameraBtn.addEventListener( "click", () => {
     activateCamera();
-
-    myCanvas.width = myVideo.videoWidth*0.5;
-    myCanvas.height = myVideo.videoHeight*0.5;
-    myCanvas.getContext('2d').drawImage(myVideo, 0, 0);
 });
 
-// alert("Hello");
 shootBtn.addEventListener( "click", () => {
-    myVideo.style.dispay = "none";
-    myCanvas.style.display = "block";
-    btnReturn.style.display = "block";
-    cameraBtn.style.display = "none";
-
+    myCanvas.width = myVideo.videoWidth;
+    myCanvas.height = myVideo.videoHeight;
+    myCanvas.getContext('2d').drawImage(myVideo, 0, 0);   
     myCanvas.toDataURL()
-
+    desactivate();
 });
 
 btnReturn.addEventListener( "click", () => {
-    myVideo.style.dispay = "block";
-    myCanvas.style.dispay = "none";
-    btnReturn.style.display = "none";
-    cameraBtn.style.display = "none";
-
+    desactivate();
 })
 
+switchCamera.addEventListener( "click", fnSwitchCamera)
+
+function switchOrientation() {
+    // const myVideo = document.querySelector('#myVideo');
+    myVideo.srcObject.getTracks().forEach(track => track.stop());
+
+    constraints = {
+        video: {
+            width: { ideal: 1920 },
+            height: { ideal: 1080 },
+            facingMode: constraints.video.facingMode
+        }
+    }
+    
+    navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+        myVideo.srcObject = stream;
+    });
+}
+
+function fnSwitchCamera() {
+    const myVideo = document.querySelector('#myVideo');
+    
+    myVideo.srcObject.getTracks().forEach(track => track.stop());
+    constraints.video.facingMode.exact = (constraints.video.facingMode.exact == "user") ? "environment" : "user";
+    navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+        myVideo.srcObject = stream;
+    });
+}
+
 function activateCamera() {
+    navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+        myVideo.srcObject = stream;
+    });
+
     cameraBtn.style.display = "none";
     btnReturn.style.display = "block";
     shootBtn.style.display = "block";
+    switchCamera.style.display = "block";
     myVideo.style.display = "block";
     myCanvas.style.display = "none";
 }
 
-function desactivateCamera(myVideo) {
-    myVideo.style.dispay = "block";
-    myCanvas.style.dispay = "none";
-    btnReturn.style.display = "none";
-    cameraBtn.style.display = "none";
-    shootBtn.style.display = "none";
-
-    
+function desactivate() {
     myVideo.srcObject.getTracks().forEach(track => track.stop());
+
+    myVideo.style.display = "none";
+    myCanvas.style.display = "block";
+    btnReturn.style.display = "none";
+    shootBtn.style.display = "none";
+    switchCamera.style.display = "none";
+    cameraBtn.style.display = "block";
 }
 
 // function whereAmI() {
