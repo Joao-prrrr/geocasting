d3b9732cb62ddc6ac6bc343e9efea67f58becc77
Joao-prrrr <joao.prrrr@eduge.ch>
2023-11-08 15:58:13 +0100

    doing gps

--- scripts/leaflet.js
+++ scripts/leaflet.js
@@ -1,3 +1,22 @@
 
+var map = L.map('map').setView(getLocation(), 13);
 
-// alert("Hello from Leaflet");
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+}).addTo(map);
+
+
+
+function getLocation() {
+    let x = document.getElementById("x");
+    x.style.display = "block";
+    navigator.geolocation.getCurrentPosition(position => {
+        let x = document.getElementById("x");
+        x.innerHTML = JSON.stringify({x: position.coords.latitude, y: position.coords.longitude});
+    });
+
+    console.log(x.innerHTML);
+    let data = x.innerHTML;
+    x.style.display = "none";
+    return [data.x, data.y];
+}
